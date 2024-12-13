e942dd5143d33a2261dcb701c89b32c4e992454e
Kyle Truong <ktruong008@gmail.com>
2024-12-13 08:24:59 -0500

    minor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -285,7 +285,7 @@
     <h2
       class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
     >
-      GM Sui
+      GM Sui (2024)
     </h2>
 
     <!-- Calendar Heatmap -->
